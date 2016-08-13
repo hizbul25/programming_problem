b5f8b24ef571ee54d6e81c9efe37ec219764c7d9
Hizbul Bahar <hizbul.ku@gmail.com>
2016-08-13 11:44:00 +0600

    fix consecutive one

--- consecutive_one.py
+++ consecutive_one.py
@@ -1,17 +1,17 @@
 # Count Consecutive one in a binary list.
 
 
+from itertools import groupby
+
+
+def len_iter(items):
+    return sum(1 for _ in items)
+
+
 def consecutive_one(data):
-    longest = 0
-    current = 0
-    for num in data:
-        if num == 1:
-            current += 1
-        elif longest < current:
-            longest = current
-            current = 0
-    return max(current, longest)
+    return max(len_iter(run) for val, run in groupby(data) if val)
 
 if __name__ == '__main__':
     data = [0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+    print(data)
     print(consecutive_one(data))
