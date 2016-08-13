0c130b22ac040d8bf01223bd89a5c5a0df48aaca
Hizbul Bahar <hizbul.ku@gmail.com>
2016-08-13 09:59:09 +0600

    fix consecutive one

--- consecutive_one.py
+++ consecutive_one.py
@@ -2,17 +2,16 @@
 
 
 def consecutive_one(data):
-    one_list = []
-    size = 0
+    longest = 0
+    current = 0
     for num in data:
         if num == 1:
-            one_list.append(num)
-        elif num == 0 and size < len(one_list):
-            size = len(one_list)
-            one_list = []
-
-    return size
+            current += 1
+        elif longest < current:
+            longest = current
+            current = 0
+    return max(current, longest)
 
 if __name__ == '__main__':
-    data = [0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0]
+    data = [0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1]
     print(consecutive_one(data))
