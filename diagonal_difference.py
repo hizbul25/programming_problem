03a203ca0ec2140f0d093650bd590ecfecc4389b
Hizbul Bahar <hizbul.ku@gmail.com>
2016-09-06 11:41:49 +0600

    hackerrank > algorithm > diagonal difference

---
+++ diagonal_difference.py
@@ -0,0 +1,15 @@
+import sys
+
+
+n = int(input().strip())
+a = []
+for a_i in range(n):
+    a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
+    a.append(a_t)
+
+sum = 0
+for i in range(n):
+    sum += a[i][i] - a[i][n-i-1]
+
+
+print(abs(sum))
