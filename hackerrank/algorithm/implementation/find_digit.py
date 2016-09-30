6fae0e3751ebccb68b1ac68185dffcc011ef5fc7
hizbul25 <hizbul.ku@gmail.com>
2016-09-30 10:04:43 +0600

    use generator instead of list

--- hackerrank/algorithm/implementation/find_digit.py
+++ hackerrank/algorithm/implementation/find_digit.py
@@ -4,4 +4,4 @@
 t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
-    print(sum([n % int(i) == 0 if int(i) > 0 else False for i in str(n)]))
+    print(sum((n % int(i) == 0 if int(i) > 0 else False for i in str(n))))
