a54af0a68f7097584fcac6e44d0cddae235f6706
hizbul25 <hizbul.ku@gmail.com>
2016-09-30 10:03:23 +0600

    algorithm > implementation > find digits

---
+++ hackerrank/algorithm/implementation/find_digit.py
@@ -0,0 +1,7 @@
+#URL: https://www.hackerrank.com/challenges/find-digits
+
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    print(sum([n % int(i) == 0 if int(i) > 0 else False for i in str(n)]))
