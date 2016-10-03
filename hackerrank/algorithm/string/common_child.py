1d46779e0fd859bf69bfc212f78ea32cad09b525
hizbul25 <hizbul.ku@gmail.com>
2016-10-03 13:58:09 +0600

    code refactor

--- hackerrank/algorithm/string/common_child.py
+++ hackerrank/algorithm/string/common_child.py
@@ -3,15 +3,16 @@
 
 def common_child(a, b):
     lengths = [[0 for j in range(len(b)+1)] for i in range(len(a)+1)]
-    for i, x in enumerate(a):
-        for j, y in enumerate(b):
-            if x == y:
-                lengths[i+1][j+1] = lengths[i][j] + 1
+    for i in range(len(a)+1):
+        for j in range(len(b)+1):
+            if i == 0 or j == 0:
+                lengths[i][j] = 0
+            elif a[i-1] == b[j-1]:
+                lengths[i][j] = lengths[i - 1][j - 1] + 1
             else:
-                lengths[i+1][j+1] = \
-                    max(lengths[i+1][j], lengths[i][j+1])
+                lengths[i][j] = max(lengths[i - 1][j], lengths[i][j - 1])
 
-    return lengths[-1][-1]
+    return lengths[len(a)][len(b)]
 
 if __name__ == '__main__':
     s1 = input().strip()
