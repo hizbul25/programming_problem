247c2e68bb8fbd1a24ad24b5701961bd9e6b1326
hizbul25 <hizbul.ku@gmail.com>
2016-10-06 10:44:44 +0600

    algorithm > implementation > cavity map

---
+++ hackerrank/algorithm/implementation/cavity_map.py
@@ -0,0 +1,16 @@
+#URL: https://www.hackerrank.com/challenges/cavity-map
+
+
+n = int(input().strip())
+grid = []
+grid_i = 0
+for _ in range(n):
+    grid_t = list(str(input().strip()))
+    grid.append(grid_t)
+for i in range(1, n - 1):
+    for j in range(1, n - 1):
+        if grid[i][j] > max(grid[i - 1][j], grid[i + 1][j], grid[i][j - 1], grid[i][j + 1]):
+            grid[i][j] = 'X'
+
+for i in range(n):
+    print(''.join(grid[i]))
