9c058c28e66f62c2944b8824efadc366aa8dde4b
Hizbul Bahar <hizbul.ku@gmail.com>
2018-01-18 00:33:57 +0600

    apple arrange

---
+++ hackerrank/algorithm/warmup/apple_orange.py
@@ -0,0 +1,16 @@
+'''
+https://www.hackerrank.com/challenges/apple-and-orange/problem
+'''
+
+if __name__ == "__main__":
+    s, t = input().strip().split(' ')
+    s, t = [int(s), int(t)]
+    a, b = input().strip().split(' ')
+    a, b = [int(a), int(b)]
+    m, n = input().strip().split(' ')
+    m, n = [int(m), int(n)]
+    apples = list(map(int, input().strip().split(' ')))
+    oranges = list(map(int, input().strip().split(' ')))
+
+    print(sum([1 for x in apples if (x + a) >= s and (x + a) <= t]))
+    print(sum([1 for x in oranges if (x + b) >= s and (x + b) <= t]))
